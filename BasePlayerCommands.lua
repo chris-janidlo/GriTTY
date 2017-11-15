89491c02a75545c7cc86b0eca53ff769a075f90b
crassSandwich <christopherjanidlo@gmail.com>
2017-11-15 18:28:55 -0500

    A PlayerComm file that runs, and also a dodge command

--- BasePlayerCommands.lua
+++ BasePlayerCommands.lua
@@ -1,28 +1,54 @@
-local IntPoint = require 'IntPoint'
+local PointField = require 'StaticIntPointField'
 
 local cmds = {}
 
 -- expansions
 cmds._e = {
 	-- wasd
-	w = IntPoint(0, -1),
-	a = IntPoint(-1, 0),
-	s = IntPoint(0, 1),
-	d = IntPoint(1, 0)
+	w = PointField(0, -1),
+	a = PointField(-1, 0),
+	s = PointField(0, 1),
+	d = PointField(1, 0)
 }
+
+function cmds.addToPlayerPos(player, point)
+	newPoint = player.location + point
+	player.location = PointField(newPoint.x, newPoint.y)
+end
 
 function cmds.move(point)
 	return {
 		{
 			function(player) player:setColor(255,0,0) end,
-			.25
+			.15
 		},
 		{
-			function(player) player:setLocation(player:getLocation() + point) end,
-			.25
+			function(player) cmds.addToPlayerPos(player, point) end,
+			.2
 		},
 		{
 			function(player) player:setColor() end,
+			0
+		}
+	}
+end
+
+function cmds.dodge(direction)
+	return {
+		{
+			function(player) player:setColor(255,0,0) end,
+			.15
+		},
+		{
+			function(player) player.invuln = true; player:setIndicator('.'); cmds.addToPlayerPos(player, direction*2) end,
+			.25
+		},
+		{
+			function(player) cmds.addToPlayerPos(player, direction); player:resetIndicator() end,
+			.5
+		},
+		{
+			function(player) player.invuln = false; player:setColor() end,
 			0
 		}
 	}
@@ -32,5 +58,10 @@ cmds.w = cmds.move(cmds._e.w)
 cmds.a = cmds.move(cmds._e.a)
 cmds.s = cmds.move(cmds._e.s)
 cmds.d = cmds.move(cmds._e.d)
+
+cmds.ww = cmds.dodge(cmds._e.w)
+cmds.aa = cmds.dodge(cmds._e.a)
+cmds.ss = cmds.dodge(cmds._e.s)
+cmds.dd = cmds.dodge(cmds._e.d)
 
 return cmds
