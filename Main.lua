1087ec226f9024bcbb1f78b7cc124ee8eefbaf4d
crassSandwich <christopherjanidlo@gmail.com>
2017-11-19 03:27:31 -0500

    window weirdness

--- Main.lua
+++ Main.lua
@@ -3,8 +3,14 @@ local Signal = require 'hump.signal'
 local Timer = require 'hump.timer'
 local CombatState = require 'CombatState'
 
+local function roundToNearest(value, rounder)
+	return math.floor(value / rounder) * rounder
+end
+
 function love.load()
 	MainFont = love.graphics.setNewFont('Monoid/Monoid-Regular.ttf')
+	local height = MainFont:getHeight()
+	love.window.setMode(roundToNearest(love.graphics.getWidth(), height), roundToNearest(love.graphics.getHeight(), height))
 	Gamestate.registerEvents()
 	Gamestate.switch(CombatState)
 end
